d7eee9c9fb27ed1d7fec28ce055c33ba9addb258
Eduard-Condurache <eduardcondurache96@gmail.com>
2024-07-11 14:55:22 +0200

    Esercizio completato

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+const kmNumber = prompt('Quale è il numero di chilometri che vuoi percorrere?');
+console.log(kmNumber);
+
+const age = prompt('Quanti anni hai?');
+console.log(age);
+
+let ticketPrice = 0.21 * kmNumber;
+
+if (age <= 17) {
+    ticketPrice = ticketPrice * 0.80;
+}
+else if (age >= 65) {
+    ticketPrice = ticketPrice * 0.60;
+}
+
+console.log('Il prezzo che devi pagare per il biglietto è: ' + ticketPrice + ' euro');
+
+document.getElementById('ticketprice-container').innerHTML = ticketPrice + 'euro'
